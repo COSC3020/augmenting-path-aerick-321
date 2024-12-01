08bb040e7dea5a0f1a61deb9e3ebd46d1227c1b4
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-12-01 13:18:15 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,7 @@
 function augmentingPath(graph, start, end) {
     if (start == end) return [start];
     if (!(start in graph)) return [];
-   /* let flow = {};
-     for (let edge in graph) {
-        flow[edge] = 0; 
-    }*/
-    
+  
     let visited = new Set();
     let path = [];
     function search (current){
